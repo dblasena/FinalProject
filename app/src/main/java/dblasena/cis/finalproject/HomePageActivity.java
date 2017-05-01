27974d1c30b8d1acac1f7d1a2d76c0710feb0a67
dblasena <dblasena@css.edu>
2017-05-01 13:58:23 -0500

    json parser

--- app/src/main/java/dblasena/cis/finalproject/HomePageActivity.java
+++ app/src/main/java/dblasena/cis/finalproject/HomePageActivity.java
@@ -1,6 +1,8 @@
 package dblasena.cis.finalproject;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,6 +12,9 @@ import android.widget.TextView;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
@@ -18,6 +23,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 import java.net.URL;
 
@@ -26,6 +32,10 @@ public class HomePageActivity extends AppCompatActivity {
     Button buttonLogOut;
     TextView textviewUser;
     String Email;
+
+    Button btnHit;
+    TextView txtJson;
+    ProgressDialog pd;
 
 
     @Override
@@ -41,6 +51,9 @@ public class HomePageActivity extends AppCompatActivity {
         Email = extras.getString("Email");
         textviewUser.setText("You are now logged as " + Email);
 
+        btnHit = (Button) findViewById(R.id.btnhit);
+        txtJson = (TextView) findViewById(R.id.txtjson);
+
 
         buttonLogOut.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -49,47 +62,89 @@ public class HomePageActivity extends AppCompatActivity {
 
 
 
+
+            }
+        });
+        btnHit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new JsonTask().execute("https://na.api.riotgames.com/api/lol/NA/v1.2/champion?freeToPlay=zac&api_key=RGAPI-e939ea0b-87b0-4e55-8103-f716a44fb6c5");
             }
         });
     }
+    private class JsonTask extends AsyncTask<String, String, String> {
 
+        protected void onPreExecute() {
+            super.onPreExecute();
 
-
-    public static JSONObject getJSONObjectFromURL(String urlString) throws IOException, JSONException {
-
-        HttpURLConnection urlConnection = null;
-
-        URL url = new URL("https://na1.api.riotgames.com/lol/platform/v3/champions?freeToPlay=zac&api_key=RGAPI-e939ea0b-87b0-4e55-8103-f716a44fb6c5");
-
-        urlConnection = (HttpURLConnection) url.openConnection();
-
-        urlConnection.setRequestMethod("GET");
-        //urlConnection.setReadTimeout(10000 /* milliseconds */);
-        //urlConnection.setConnectTimeout(15000 /* milliseconds */);
-
-        urlConnection.setDoOutput(true);
-
-        urlConnection.connect();
-
-        BufferedReader br=new BufferedReader(new InputStreamReader(url.openStream()));
-
-        char[] buffer = new char[1024];
-
-        String jsonString = new String();
-
-        StringBuilder sb = new StringBuilder();
-        String line;
-        while ((line = br.readLine()) != null) {
-            sb.append(line+"\n");
+            pd = new ProgressDialog(HomePageActivity.this);
+            pd.setMessage("Please wait");
+            pd.setCancelable(false);
+            pd.show();
         }
-        br.close();
 
-        jsonString = sb.toString();
+        protected String doInBackground(String... params) {
 
-        System.out.println("JSON: " + jsonString);
 
-        return new JSONObject(jsonString);
+            HttpURLConnection connection = null;
+            BufferedReader reader = null;
+
+            try {
+                URL url = new URL(params[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+
+
+                InputStream stream = connection.getInputStream();
+
+                reader = new BufferedReader(new InputStreamReader(stream));
+
+                StringBuffer buffer = new StringBuffer();
+                String line = "";
+
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line+"\n");
+                    Log.d("Response: ", "> " + line);
+
+                }
+
+                return buffer.toString();
+
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+                try {
+                    if (reader != null) {
+                        reader.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            if (pd.isShowing()){
+                pd.dismiss();
+            }
+            txtJson.setText(result);
+        }
     }
+
+
+
+
+
+
 
 
     public void finish() {
